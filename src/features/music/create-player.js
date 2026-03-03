40354bfea230d9c6afab6b3d45539874d87cfdc5
tsu <hi@tsu.ar>
2026-03-02 22:31:34 -0300

    feat: some performance improvements

--- src/features/music/create-player.js
+++ src/features/music/create-player.js
@@ -6,9 +6,31 @@ export function createPlayer(client) {
   return new DisTube(client, {
     plugins: [
       new SpotifyPlugin(),
-      new YouTubePlugin(),
+      new YouTubePlugin({
+        ytdlOptions: {
+          quality: 'lowestaudio',
+          filter: 'audioonly',
+          highWaterMark: 1024 * 1024 * 2, // 2MB buffer
+        },
+      }),
     ],
+    leaveOnEmpty: true,
+    leaveOnFinish: true,
+    leaveOnStop: true,
     emitAddListWhenCreatingQueue: true,
     emitAddSongWhenCreatingQueue: false,
+    ffmpeg: {
+      args: {
+        global: ['-hide_banner'],
+        input: ['-reconnect', '1', '-reconnect_streamed', '1', '-reconnect_delay_max', '5'],
+        output: [
+          '-ac', '2',
+          '-ar', '48000',
+          '-b:a', '64k',
+          '-map', '0:a',
+          '-vn',
+        ],
+      },
+    },
   })
 }
